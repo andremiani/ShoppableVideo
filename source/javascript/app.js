9c282274fb55167f8cd6e22196a22dd3c8e0e79c
Mattias Cederberg <oi14mcg@cs.umu.se>
2017-05-09 19:36:12 +0200

    Add product draggable/sortable functionality

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -1,27 +1,33 @@
-// Declare the shoppableVideo module and its dependency 'ui.bootstrap'
-var app = angular.module('shoppableVideo', ['ui.bootstrap']);
+// Declare the shoppableVideo module and its dependencies
+var app = angular.module('shoppableVideo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui.sortable']);
 // Declare the AppCtrl controller
 app
     .controller('AppCtrl', ['$scope', function($scope) {
 
-        // Card counter
+        // Card counter, used for card naming
         var cardCounter = 1;
 
+        // Product counter, used for product naming
         var productCounter = "";
 
         // Variable for the video title
         $scope.videoTitle = "";
 
-
         // Array to store the productCards
         $scope.productCards = [];
-
 
         //Collapse variable for the product cards, true if collapsed
         $scope.isCollapsed = false;
 
         //Collaspse variable for the products, true if collapsed
         $scope.isProductCollapsed = false;
+
+        //Tells ui-sortable what and how to sort
+        $scope.sortableOptions = {
+            handle: '.product',
+            // items: ' .panel:not(.panel-heading)'
+            axis: 'y'
+        };
 
         // Add productCard to the end of the array
         var addProductCard = function() {
@@ -38,7 +44,7 @@ app
         var addProduct = function(index) {
             productCounter = $scope.productCards[index].products.length + 1;
             $scope.productCards[index].products.push({
-                productTitle: 'Product ' + productCounter ,
+                productTitle: 'Product ' + productCounter,
                 link: 'https://www...',
                 specialOffer: '20%',
                 buttonText: 'Buy now baby',
@@ -73,43 +79,4 @@ app
             addProductCard();
         }*/
 
-
-    }])
-    .directive('tabHighlight', [function() {
-
-        return {
-            restrict: 'A',
-            link: function(scope, element) {
-                // Here is the major jQuery usage where we add the event
-                // listeners mousemove and mouseout on the tabs to initalize
-                // the moving highlight for the inactive tabs
-                var x, y, initial_background = '#c3d5e6';
-
-                element
-                    .removeAttr('style')
-                    .mousemove(function(e) {
-                        // Add highlight effect on inactive tabs
-                        if (!element.hasClass('active')) {
-                            x = e.pageX - this.offsetLeft;
-                            y = e.pageY - this.offsetTop;
-
-                            // Set the background when mouse moves over inactive tabs
-                            element
-                                .css({
-                                    background: '-moz-radial-gradient(circle at ' + x + 'px ' + y + 'px, rgba(255,255,255,0.4) 0px, rgba(255,255,255,0.0) 45px), ' + initial_background
-                                })
-                                .css({
-                                    background: '-webkit-radial-gradient(circle at ' + x + 'px ' + y + 'px, rgba(255,255,255,0.4) 0px, rgba(255,255,255,0.0) 45px), ' + initial_background
-                                })
-                                .css({
-                                    background: 'radial-gradient(circle at ' + x + 'px ' + y + 'px, rgba(255,255,255,0.4) 0px, rgba(255,255,255,0.0) 45px), ' + initial_background
-                                });
-                        }
-                    })
-                    .click(function() {
-                        // Return the inital background color of the tab
-                        element.removeAttr('style');
-                    });
-            }
-        };
     }]);
